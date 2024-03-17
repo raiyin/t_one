7f6ad413e7303a1232cba11a4a1301c77666495b
Maxim P. <raiyin@ya.ru>
2024-03-17 19:03:48 +0300

    ts-2 Review pp done

--- t-one/src/components/organisms/ReviewBlock/ReviewBlock.tsx
+++ t-one/src/components/organisms/ReviewBlock/ReviewBlock.tsx
@@ -6,14 +6,17 @@ import DividerImage from '../../../assets/images/pngs/divider.png'
 const ReviewBlock = () => {
     return (
         <div className={styles['review_container']}>
-            <Title
-                text={'Customer Say'}
-                selectStart={0}
-                selectEnd={8}
-                fontSize={45}
-                fontWeight={600}
-                lineHeight={26}
-            />
+            <div className={styles['review_title_wrapper']}>
+
+                <Title
+                    text={'Customer Say'}
+                    selectStart={0}
+                    selectEnd={8}
+                    fontSize={45}
+                    fontWeight={600}
+                    lineHeight={26}
+                />
+            </div>
             <ul className={styles['reviews']}>
                 <CommentWidget user_handle={'@omottley'} comment={'I cannot believe how I found you, this is so pretty.'} selected />
                 <CommentWidget user_handle={'@omottley'} comment={'I cannot believe how I found you, this is so pretty.'} selected={false} />
