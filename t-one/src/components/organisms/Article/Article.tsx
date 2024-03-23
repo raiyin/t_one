ef349eeddaadad461eac53b64b5de8471659749c
Maxim P. <raiyin@ya.ru>
2024-03-23 09:20:26 +0300

    ts-3 article organism added

--- t-one/src/components/organisms/Article/Article.tsx
+++ t-one/src/components/organisms/Article/Article.tsx
@@ -1,12 +1,32 @@
 import { ArticleProps } from '../../../types/ArticleProps'
 import Title from '../../atoms/Title/Title'
+import ArticleHead from '../../molecules/ArticleHead/ArticleHead'
 import styles from './Article.module.css'
 
 const Article = (props: ArticleProps) => {
     return (
-        <div>
+        <div className={styles['article']}>
+            <div className={styles['article-title']}>
+                <Title
+                    text={props.article_title}
+                    selectStart={0}
+                    selectEnd={0}
+                    fontSize={40}
+                    fontWeight={600}
+                    lineHeight={45} />
+            </div>
 
-            <Title text={props.article_title} selectStart={0} selectEnd={0} fontSize={40} fontWeight={600} lineHeight={45} />
+            <div className={styles['article-head']}>
+                <ArticleHead
+                    author={props.author}
+                    ava_url={props.ava_url}
+                    rating={props.rating}
+                    hashtag={props.hashtag} />
+            </div>
+
+            <div className={styles['article-text']}>
+                {props.article_text}
+            </div>
         </div>
     )
 }
